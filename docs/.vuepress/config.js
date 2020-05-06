e10460781f9e416c2a23d24a6a640f5619d8aeac
songning0605 <songningning0605@gmail.com>
2020-05-07 00:40:26 +0800

    测试菜单潜逃

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,12 +7,32 @@ module.exports = {
       { text: 'Home', link: '/' },
       { text: 'External', link: 'https://google.com' },
     ],
-    sidebar: {
-      '/': [
-        '',        /* / */
-        'contact', /* /contact.html */
-        'about'    /* /about.html */
-      ],
-    }
+    sidebar: [
+      {
+        title: 'Group 1',   // 必要的
+        path: '/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+        children: [
+          '/'
+        ]
+      },
+      {
+        title:'前端1',
+        collapsable: false,
+        children:[
+          '/frontEnd/测试1',
+          '/frontEnd/测试2',
+        ]
+      },
+      {
+        title:'前端2',
+        collapsable: false,
+        children:[
+          '/frontEnd/测试1',
+          '/frontEnd/测试2',
+        ]
+      },
+    ]
   }
 }
---
+++ utils/autoPath.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const path = require('path')
+
+function checkFileType(path) {
+  return path.includes(".md")
+}
+
+function prefixPath(basePath,dirPath) {
+  let index = basePath.indexOf("/")
+  // 去除一级目录地址
+  basePath = basePath.slice(index,path.length)
+  // replace用于处理windows电脑的路径用\表示的问题
+  return path.join(basePath,dirPath).replace(/\\/g,"/")
+}
+
+function readDirSync(path,root){
+  var pa = fs.readdirSync(path);
+  pa.forEach(function(ele,index){
+    var info = fs.statSync(path+"/"+ele)
+    if(info.isDirectory()) {
+      readDirSync(path+ele,root)
+    } else {
+      if (checkFileType(ele)) {
+        root.push(prefixPath(path,ele))
+      }
+    }
+  })
+}
+
+function getChildren(path) {
+  const root = []
+  readDirSync(path,root)
+  return root
+}
+
+
+module.exports = {
+  getChildren:getChildren
+}
